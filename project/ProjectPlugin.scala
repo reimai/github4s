c15ba24b382fde61e81dfb84945c37a895cc12bf
47erbot <developer@47deg.com>
2020-08-07 07:08:46 +0200

    Update scalatest to 3.2.1

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -20,7 +20,7 @@ object ProjectPlugin extends AutoPlugin {
       val http4s: String    = "0.21.6"
       val paradise: String  = "2.1.1"
       val scalamock: String = "5.0.0"
-      val scalatest: String = "3.2.0"
+      val scalatest: String = "3.2.1"
       val silencer: String  = "1.7.1"
     }
 
