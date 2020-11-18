abf58ea40f918c048d39881909225627543b1e66
47erbot <developer@47deg.com>
2020-11-18 06:10:35 +0100

    Update http4s-blaze-client, http4s-circe, ... to 0.21.9

--- project/ProjectPlugin.scala
+++ project/ProjectPlugin.scala
@@ -17,7 +17,7 @@ object ProjectPlugin extends AutoPlugin {
       val base64: String    = "0.2.10"
       val cats: String      = "2.2.0"
       val circe: String     = "0.13.0"
-      val http4s: String    = "0.21.8"
+      val http4s: String    = "0.21.9"
       val paradise: String  = "2.1.1"
       val scalamock: String = "5.0.0"
       val scalatest: String = "3.2.2"
